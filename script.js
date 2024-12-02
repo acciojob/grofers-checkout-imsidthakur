8266312c7d6a47a38831a92b295935a3b5046284
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-12-02 21:44:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const getSumBtn = document.createElement("button");
+const table = document.getElementsByTagName('table')[0];
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+ const  prices = document.querySelectorAll(".price");
+ let sum = 0;
+ 
+
+ prices.forEach((num) => {
+	 sum += Number(num.textContent);
+ })
+
+ const row = document.createElement("tr");
+ const col1 = document.createElement("td");
+ col1.setAttribute("colspan",2);
+ col1.textContent = sum;
+
+ row.append(col1);
+ table.append(row);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
